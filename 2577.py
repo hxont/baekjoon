2dba8bb8a13d80fca1b4ea9d1e1c3ad490465769
hoont <queenly9@icloud.com>
2025-02-05 20:37:34 +0900

    골드가야함

---
+++ 2577.py
@@ -0,0 +1,31 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+multi = a * b * c
+
+zero = str(multi).count("0")
+one = str(multi).count("1")
+two = str(multi).count("2")
+three = str(multi).count("3")
+four = str(multi).count("4")
+five = str(multi).count("5")
+six = str(multi).count("6")
+seven = str(multi).count("7")
+eight = str(multi).count("8")
+nine = str(multi).count("9")
+
+print(zero)
+print(one)
+print(two)
+print(three)
+print(four)
+print(five)
+print(six)
+print(seven)
+print(eight)
+print(nine)
+
+
+
+
