c3a9b89514a4c48e161b2a0fa417c683a8c14df8
hoont <queenly9@icloud.com>
2025-02-06 23:43:39 +0900

    골드가야함

---
+++ 4101.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        a, b = map(int, input().split())
+
+        if a == 0 and b == 0:
+            break
+
+        elif a > b:
+            print('Yes')
+        elif a == b:
+            print('No')
+        elif a < b:
+            print('No')
+    except :
+        break
